235dd53abdfb1a2a16cc281d91116d4a17e01adf
emerson-oliveira-clear <emerson.oliveira.clear@gmail.com>
2023-11-14 04:32:08 -0300

    firt commit

---
+++ app/helpers/articles_helper.rb
@@ -0,0 +1,6 @@
+module ArticlesHelper
+    def month_day_year(datetime)
+        datetime.strftime('%B %e, %Y')
+    end
+
+end
---
+++ app/javascript/application.js
@@ -0,0 +1,14 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+
+
+require("@popperjs/core")
+require("../stylesheets/application.scss")
+
+document.addEventListener("turbolinks:load", () => {
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new Tooltip(tooltipTriggerEl))
+})
+
+import { Tooltip } from "bootstrap"
